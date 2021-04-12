72f9e65255147f8ea1394544b1d06a45a18d2c13
kivan117 <melliott117@gmail.com>
2021-04-11 20:38:05 -0700

    Cpu status flags and timing table

--- inc/Cpu.h
+++ inc/Cpu.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <cstdint>
 #include "Mmu.h"
+#include "Logger.h"
 
 class Cpu
 {
 public:
+	Cpu(Mmu* _mmu);
 	void Tick();
 private:
+	Mmu* mmu{ nullptr };
+
+	uint64_t totalCycles{ 0 };
+
+	uint8_t opCycles{ 0 };
+
 	struct registers {
 		uint8_t   A;	// Accumulator
 		uint8_t   X;	// X index
@@ -15,5 +23,41 @@ private:
 		uint8_t   S;	// Stack pointer
 		uint16_t PC;	// Program counter
 	} Regs;
+
+	struct statusflags {
+		bool negative{ false };
+		bool overflow{ false };
+		bool brk_interrupt{ false };
+		bool decimal{ false };
+		bool irq_disable{ false };
+		bool zero{ false };
+		bool carry{ false };
+	} Flags;
+
+	enum FLAG_CODE { FLAG_C = 0, FLAG_Z, FLAG_I, FLAG_D, FLAG_B, FLAG_HIGH, FLAG_V, FLAG_N };
+
+	uint8_t Execute(uint8_t op);
+
+	void SetFlag(FLAG_CODE flagCode, uint8_t val);
+
+	uint8_t BaseCyclesLUT[256]{
+	//  0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+		7, 6, 0, 8, 3, 3, 5, 5, 3, 2, 2, 2, 4, 4, 6, 6, // 0
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7, // 1
+		6, 6, 0, 8, 3, 3, 5, 5, 4, 2, 2, 2, 4, 4, 6, 6, // 2
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4 ,7, 7, // 3
+		6, 6, 0, 8, 3, 3, 5, 5, 3, 2, 2, 2, 3, 4, 6, 6, // 4
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7, // 5
+		6, 6, 0, 8, 3, 3 ,5, 5, 4, 2, 2, 2, 5, 4, 6, 6, // 6
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7, // 7
+		2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4, // 8
+		2, 6, 0, 6, 4, 4, 4, 4, 2, 5, 2, 5, 5, 5, 5, 5, // 9
+		2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4, // A
+		2, 5, 0, 5, 4, 4, 4, 4, 2, 4, 2, 4, 4, 4, 4, 4, // B
+		2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6, // C
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7, // D
+		2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6, // E
+		2, 5, 0, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7  // F
+	};
 };
 
--- src/Cpu.cpp
+++ src/Cpu.cpp
@@ -1,5 +1,71 @@
 #include "Cpu.h"
 
+Cpu::Cpu(Mmu* _mmu) : mmu(_mmu)
+{
+    Regs.A  = 0x00;
+    Regs.X  = 0x00;
+    Regs.Y  = 0x00;
+    Regs.P  = 0x34;
+    Regs.S  = 0xFD;
+    Regs.PC = 0xC000; //for auto nestest. normal reset vector is 0xFFFC
+}
+
 void Cpu::Tick()
 {
+    //fetch
+    uint8_t op = mmu->ReadByte(Regs.PC++);
+    //decode-execute
+    opCycles = Execute(op);
+    totalCycles += opCycles;
+}
+
+uint8_t Cpu::Execute(uint8_t op)
+{
+    uint8_t tempCycles = BaseCyclesLUT[op];
+    switch (op)
+    {
+    case(0xEA):
+        //NOP
+        break;
+    default:
+        LOG_ERROR("Invalid opcode: {}", op);
+        exit(-1);
+    }
+
+    return tempCycles;
+}
+
+void Cpu::SetFlag(FLAG_CODE flagCode, uint8_t val)
+{
+    switch (flagcode)
+    {
+    case(FLAG_C):
+        Flags.carry = (val != 0) ? true : false;
+        break;
+    case(FLAG_Z):
+        Flags.zero = (val != 0) ? true : false;
+        break;
+    case(FLAG_I):
+        Flags.irq_disable = (val != 0) ? true : false;
+        break;
+    case(FLAG_D):
+        Flags.decimal = (val != 0) ? true : false;
+        break;
+    case(FLAG_B):
+        Flags.brk_interrupt = (val != 0) ? true : false;
+        break;
+    case(FLAG_V):
+        Flags.overflow = (val != 0) ? true : false;
+        break;
+    case(FLAG_N):
+        Flags.negative = (val != 0) ? true : false;
+        break;
+    default:
+        LOG_ERROR("Attempt to set improper flag: {}", flagCode);
+        exit(-1);
+    }
+
+    uint8_t mask = 1 << flagCode;
+    Regs.P &= ~(mask);
+    Regs.P |= (val != 0) ? mask : 0;
 }
